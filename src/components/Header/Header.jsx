d2f327fdf44f8b9fe25e7e8adae55add3d21f8a0
sylsylviane <syl.sylviane1@gmail.com>
2025-02-26 16:58:09 -0500

    Enlever l'import react

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+// IMPORTER LES STYLES CSS DU HEADER ICI
 import Nav from "../Nav/Nav";
-function Header(){
-    return (
-        <header>
-            <Nav />
-        </header>
-    )
+
+function Header() {
+  return (
+    <header>
+      <Nav />
+    </header>
+  );
 }
 
 export default Header;
