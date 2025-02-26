ed4388a4d2964b43449fb4f3d07890927fce72b6
sylsylviane <syl.sylviane1@gmail.com>
2025-02-26 17:00:48 -0500

    Modifier le commentaire

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App/App'
+// IMPORTER LES STYLES CSS DU FICHIER STYLES.CSS ICI
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App/App';
+import { BrowserRouter } from 'react-router-dom';
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
