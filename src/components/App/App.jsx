0713352eaa8068d89b757026f2d324d982d8b52d
sylsylviane <syl.sylviane1@gmail.com>
2025-02-26 16:53:40 -0500

    Ajouter les routes pour la navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import Header from '../Header/Header';
+// IMPORTER LES STYLES CSS DE L'APP ICI
 
-function App(){
-    return (
-        <Header />
+import { Route, Routes } from "react-router-dom";
 
-    )
+import Header from "../Header/Header";
+import Accueil from "../Accueil/Accueil";
+import ListeLivres from "../ListeLivres/ListeLivres";
+import CarteLivre from "../CarteLivre/CarteLivre";
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/livres" element={<ListeLivres />} />
+        <Route path="/livres/:id" element={<CarteLivre />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
