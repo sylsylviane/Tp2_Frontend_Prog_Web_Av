61d74ae9ec2dc69a0e1cd231878759664c8b1782
sylsylviane <syl.sylviane1@gmail.com>
2025-02-26 16:24:19 -0500

    Créer le composant Nav pour la navigation

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+    return (
+      <nav>
+        <a href="#">Mes livres</a>
+        <a href="#">Explorer</a>
+        <a href="#">Se connecter</a>
+        <a href="#">S'inscrire</a>
+        <a href="#">Mon profil</a>
+      </nav>
+    );
+}
+
+export default Nav;
