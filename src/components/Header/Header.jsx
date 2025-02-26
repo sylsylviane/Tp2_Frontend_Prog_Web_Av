886a6e172413437ced075dd52e75154f6fbaea27
sylsylviane <syl.sylviane1@gmail.com>
2025-02-26 16:24:51 -0500

    Créer le composant Header pour l'entête

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from "../Nav/Nav";
+function Header(){
+    return (
+        <header>
+            <Nav />
+        </header>
+    )
+}
+
+export default Header;
