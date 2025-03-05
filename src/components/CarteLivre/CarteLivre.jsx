84da7c8f4167154feadebc891a6228de336881d3
sylsylviane <syl.sylviane1@gmail.com>
2025-03-05 14:36:34 -0500

    Ajouter commentaire au code

--- src/components/CarteLivre/CarteLivre.jsx
+++ src/components/CarteLivre/CarteLivre.jsx
@@ -2,15 +2,19 @@
 
 import { useNavigate } from "react-router-dom";
 
+// Composant qui permet d'afficher la carte d'un livre
 function CarteLivre(props) {
   const {livre = {id: "", titre: ""}} = props;
   
   let navigate = useNavigate()
 
+  // Fonction qui permet de naviguer vers la page de détail d'un livre
   function cliqueCarte(event) {
     const declencheur = event.currentTarget;
     navigate(`/livres/${declencheur.id}`);
   }
+
+  // Afficher la carte d'un livre
   return (
     <div key={`livre-${livre.id}`} onClick={cliqueCarte} id={livre.id}>     
       <img src={`img/${livre.image}`} alt="{livre.titre}" />
