edb01746753b76279b38ac98a1c7bfd50465a87b
sylsylviane <syl.sylviane1@gmail.com>
2025-03-05 14:23:32 -0500

    Récupérer la liste des livres depuis le backend

--- src/components/ListeLivres/ListeLivres.jsx
+++ src/components/ListeLivres/ListeLivres.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+// TODO: IMPORTER CSS 
 
+import { useState, useEffect } from "react";
+import CarteLivre from "../CarteLivre/CarteLivre";
+
+// Composant qui permet d'afficher la liste des livres
 function ListeLivres(){
+  
+    let [livres, setLivres] = useState([]);
+
+    // Récupérer les livres de la base de données
+    useEffect(() => {
+      async function fetchData() {
+        let url = import.meta.env.VITE_DEV_URL;
+        
+        if (import.meta.env.VITE_MODE == "PRODUCTION") {
+          url = import.meta.env.VITE_PROD_URL;
+        }
+
+        const reponse = await fetch(`${url}/livres`);
+        const donneesLivres = await reponse.json();
+        setLivres(donneesLivres);
+      }
+      fetchData();
+    }, []);
+
     return (
         <div>
             <h1>Liste de livres</h1>
+            {livres.map((livre) => {
+                return <CarteLivre key={livre.id} livre={livre}/>
+            })}
+            {livres.length == 0 && "Aucun livre trouvé."}
         </div>
     )
 }
