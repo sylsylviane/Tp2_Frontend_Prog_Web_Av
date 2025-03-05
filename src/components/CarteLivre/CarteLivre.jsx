dc6de8d12cc5bcb2751cc07e887019215c1f5bbb
sylsylviane <syl.sylviane1@gmail.com>
2025-03-05 14:25:38 -0500

    Créer composant pour afficher la carte d'un livre

--- src/components/CarteLivre/CarteLivre.jsx
+++ src/components/CarteLivre/CarteLivre.jsx
@@ -1,9 +1,19 @@
 // IMPORTER LES STYLES CSS DE LA CARTE D'UN LIVRE ICI
 
-function CarteLivre() {
+import { useNavigate } from "react-router-dom";
+
+function CarteLivre(props) {
+  const {livre = {id: "", titre: ""}} = props;
+  
+  let navigate = useNavigate()
+
+  function cliqueCarte(event) {
+    const declencheur = event.currentTarget;
+    navigate(`/livres/${declencheur.id}`);
+  }
   return (
-    <div>
-      <h1>Livre</h1>
+    <div key={`livre-${livre.id}`} onClick={cliqueCarte} id={livre.id}>     
+      <img src={`img/${livre.image}`} alt="{livre.titre}" />
     </div>
   );
 }
