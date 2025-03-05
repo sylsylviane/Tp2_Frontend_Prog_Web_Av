1697a3b692859bb9bf3656fc49eb1e5341ff0b2b
sylsylviane <syl.sylviane1@gmail.com>
2025-03-05 15:24:17 -0500

    Ajouter he pour décoder les caractères spéciaux lors de l'affichage des détails d'un livre

--- src/components/DetailLivre/DetailLivre.jsx
+++ src/components/DetailLivre/DetailLivre.jsx
@@ -1,98 +1,100 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-
+import he from "he";
 // Composant qui permet d'afficher les détails d'un livre
 function DetailFilm (){
-    const {id} = useParams();
-    let [livre, setLivre] = useState({
-        titre:"",
-        description:"",
-        editeur:"",
-        isbn:"",
-        pages:"",
-        date:"",
-        image:"",
-        categories:[],
-        auteur:""
-    });
+  const { id } = useParams();
+  let [livre, setLivre] = useState({
+    titre: "",
+    description: "",
+    editeur: "",
+    isbn: "",
+    pages: "",
+    date: "",
+    image: "",
+    categories: [],
+    auteur: "",
+  });
+  // Fonction qui permet de décoder les caractères spéciaux
+  const d = (text) => he.decode(text);
 
-    // Récupérer les détails d'un livre de la base de données
-    useEffect(() => {
-      async function fetchData() {
-        let url = import.meta.env.VITE_DEV_URL;
-
-        if (import.meta.env.VITE_MODE == "PRODUCTION") {
-          url = import.meta.env.VITE_PROD_URL;
-        }
-
-        const reponse = await fetch(`${url}/livres/${id}`);
-        const donneesLivre = await reponse.json();
-        setLivre(donneesLivre);
-      }
-      fetchData();
-    }, []);
-
-    // Fonction qui permet de supprimer un livre
-    async function supprimerLivre() {
+  // Récupérer les détails d'un livre de la base de données
+  useEffect(() => {
+    async function fetchData() {
       let url = import.meta.env.VITE_DEV_URL;
 
       if (import.meta.env.VITE_MODE == "PRODUCTION") {
         url = import.meta.env.VITE_PROD_URL;
       }
 
-      const objDonnees = {
-        method: "DELETE",
-      };
-      const reponse = await fetch(`${url}/livres/${id}`, objDonnees);
-      if (reponse.ok) {
-        navigate("/livres");
-      } 
+      const reponse = await fetch(`${url}/livres/${id}`);
+      const donneesLivre = await reponse.json();
+      setLivre(donneesLivre);
+    }
+    fetchData();
+  }, []);
+
+  // Fonction qui permet de supprimer un livre
+  async function supprimerLivre() {
+    let url = import.meta.env.VITE_DEV_URL;
+
+    if (import.meta.env.VITE_MODE == "PRODUCTION") {
+      url = import.meta.env.VITE_PROD_URL;
     }
 
-    // Afficher les détails d'un livre
-    return (
-      <main>
-        <div>
-          <img src={`/img/${livre.image}`} alt="" />
-          <p>
-            <strong>Titre: </strong>
-            {livre.titre}
-          </p>
-          <p>
-            <strong>Description: </strong>
-            {livre.description}
-          </p>
-          <p>
-            <strong>Éditeur: </strong>
-            {livre.editeur}
-          </p>
-          <p>
-            <strong>ISBN: </strong>
-            {livre.isbn}
-          </p>
-          <p>
-            <strong>Nombre de pages: </strong>
-            {livre.pages}
-          </p>
-          <p>
-            <strong>Date de publication: </strong>
-            {livre.date}
-          </p>
-          <p>
-            <strong>Catégories: </strong>
-            {livre.categories.join(", ")}
-          </p>
-          <p>
-            <strong>Auteur: </strong>
-            {livre.auteur}
-          </p>
+    const objDonnees = {
+      method: "DELETE",
+    };
+    const reponse = await fetch(`${url}/livres/${id}`, objDonnees);
+    if (reponse.ok) {
+      navigate("/livres");
+    }
+  }
 
-          <div onClick={supprimerLivre}>Supprimer</div>
+  // Afficher les détails d'un livre
+  return (
+    <main>
+      <div>
+        <img src={`/img/${livre.image}`} alt="" />
+        <p>
+          <strong>Titre: </strong>
+          {d(livre.titre)}
+        </p>
+        <p>
+          <strong>Description: </strong>
+          {d(livre.description)}
+        </p>
+        <p>
+          <strong>Éditeur: </strong>
+          {d(livre.editeur)}
+        </p>
+        <p>
+          <strong>ISBN: </strong>
+          {d(livre.isbn)}
+        </p>
+        <p>
+          <strong>Nombre de pages: </strong>
+          {d(livre.pages)}
+        </p>
+        <p>
+          <strong>Date de publication: </strong>
+          {d(livre.date)}
+        </p>
+        <p>
+          <strong>Catégories: </strong>
+          {d(livre.categories.join(", "))}
+        </p>
+        <p>
+          <strong>Auteur: </strong>
+          {d(livre.auteur)}
+        </p>
 
-          <Link to="/livres">Retour aux livres</Link>
-        </div>
-      </main>
-    );
+        <div onClick={supprimerLivre}>Supprimer</div>
+
+        <Link to="/livres">Retour aux livres</Link>
+      </div>
+    </main>
+  );
 }
 
 export default DetailFilm
