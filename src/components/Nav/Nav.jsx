e72bf07ae24338d2d23b9228ea7a6bdc1da354be
sylsylviane <syl.sylviane1@gmail.com>
2025-03-11 15:47:26 -0400

    Créer menu de navigation responsive avec tailwind

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,102 @@
 
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+const navigation = [
+  { name: "Accueil", href: "/" },
+  { name: "Liste des livres", href: "/livres" },
+  { name: "Ajouter un livre", href: "livres/ajout" },
 
+];
 function Nav() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <nav>
-      <NavLink to="/">Accueil</NavLink>
-      <NavLink to="/livres">Liste des livres</NavLink>
-      <NavLink to="livres/ajout">Ajouter un livre</NavLink>
-    </nav>
+    <>
+      <nav className="flex items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1">
+          <NavLink to="/">
+            <h1 className="d-block text-2xl font-bold text-neutral-50">
+              <strong>Libra </strong>
+              <small>System</small>
+            </h1>
+          </NavLink>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+          >
+            <span className="sr-only">Ouvrir le menu principal</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <NavLink
+              key={item.name}
+              to={item.href}
+              className="text-neutral-50 hover:text-neutral-200"
+            >
+              {item.name}
+            </NavLink>
+          ))}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm/6 ">
+            Se connecter <span>&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <h1 className="d-block text-2xl font-bold text-gray-700">
+              <strong>Libra </strong>
+              <small>System</small>
+            </h1>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Fermer le menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Se connecter
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </>
   );
 }
 
