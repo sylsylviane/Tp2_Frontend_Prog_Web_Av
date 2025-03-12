10d0204df0013cabb88737d9b75aea1f300f9ad8
sylsylviane <syl.sylviane1@gmail.com>
2025-03-12 17:22:14 -0400

    Ajouter style css et animation des boutons dans le détail d'un livre

--- src/components/DetailLivre/DetailLivre.jsx
+++ src/components/DetailLivre/DetailLivre.jsx
@@ -1,8 +1,9 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import he from "he";
+import { motion } from "motion/react";
 // Composant qui permet d'afficher les détails d'un livre
-function DetailFilm (){
+function DetailLivre (){
   const { id } = useParams();
   let [livre, setLivre] = useState({
     titre: "",
@@ -54,47 +55,62 @@ function DetailFilm (){
   // Afficher les détails d'un livre
   return (
     <main>
-      <div>
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
         <img src={`/img/${livre.image}`} alt="" />
-        <p>
-          <strong>Titre: </strong>
-          {d(livre.titre)}
-        </p>
-        <p>
-          <strong>Description: </strong>
-          {d(livre.description)}
-        </p>
-        <p>
-          <strong>Éditeur: </strong>
-          {d(livre.editeur)}
-        </p>
-        <p>
-          <strong>ISBN: </strong>
-          {d(livre.isbn)}
-        </p>
-        <p>
-          <strong>Nombre de pages: </strong>
-          {d(livre.pages)}
-        </p>
-        <p>
-          <strong>Date de publication: </strong>
-          {d(livre.date)}
-        </p>
-        <p>
-          <strong>Catégories: </strong>
-          {d(livre.categories.join(", "))}
-        </p>
-        <p>
-          <strong>Auteur: </strong>
-          {d(livre.auteur)}
-        </p>
-
-        <div onClick={supprimerLivre}>Supprimer</div>
-
-        <Link to="/livres">Retour aux livres</Link>
+        <div>
+          <h2 className="text-3xl">{d(livre.titre)}</h2>
+          <p className="mt-4">{d(livre.description)}</p>
+          <dl className="mt-16 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+            <div className="border-t border-gray-200 pt-4">
+              <dt className="font-medium">Auteur</dt>
+              <dd className="mt-2 text-sm">{d(livre.auteur)}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-4">
+              <dt className="font-medium">Éditeur</dt>
+              <dd className="mt-2 text-sm">{d(livre.editeur)}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-4">
+              <dt className="font-medium">ISBN</dt>
+              <dd className="mt-2 text-sm">{d(livre.isbn)}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-4">
+              <dt className="font-medium">Nombre de pages</dt>
+              <dd className="mt-2 text-sm">{d(livre.pages)}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-4">
+              <dt className="font-medium">Date de publication</dt>
+              <dd className="mt-2 text-sm">{d(livre.date)}</dd>
+            </div>
+            <div className="border-t border-gray-200 pt-4">
+              <dt className="font-medium">Catégorie(s)</dt>
+              <dd className="mt-2 text-sm">{d(livre.categories.join(", "))}</dd>
+            </div>
+          </dl>
+          <div className="flex mt-8 gap-4 justify-between">
+            <motion.div
+              whileHover={{ scale: 1.1, transition: { duration: 1 } }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <Link
+                to="/livres"
+                className="border border-red-400 rounded-md text-gray-700 px-3.5 py-2.5 text-lg text-neutral-50 hover:bg-red-400"
+              >
+                Retour aux livres
+              </Link>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1, transition: { duration: 1 } }}
+              whileTap={{ scale: 0.9 }}
+              className="font-[Cormorant_Garamond] uppercase bg-red-800 px-3.5 py-2.5 text-lg rounded-md text-white hover:bg-red-400"
+              onClick={supprimerLivre}
+            >
+              Supprimer
+            </motion.div>
+          </div>
+        </div>
       </div>
     </main>
   );
 }
 
-export default DetailFilm
+export default DetailLivre
