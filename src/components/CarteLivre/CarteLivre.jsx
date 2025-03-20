ee48d779628c364d662ddb5d25168f3da331cecf
sylsylviane <syl.sylviane1@gmail.com>
2025-03-20 17:17:19 -0400

    Ajouter div autour de l'image et de l'envers de la carte pour mieux ajuster l'image

--- src/components/CarteLivre/CarteLivre.jsx
+++ src/components/CarteLivre/CarteLivre.jsx
@@ -46,31 +46,33 @@ function CarteLivre(props) {
       whileHover="survol"
       exit="normal"
       variants={etats}
-      className="relative min-h-[550px] min-w-[350px] max-w-[400px] overflow-hidden cursor-pointer"
+      className="relative min-h-[550px] min-w-[350px] max-w-[400px] overflow-hidden cursor-pointer "
       key={`livre-${livre.id}`}
       onClick={cliqueCarte}
       id={livre.id}
     >
-      <motion.img
-        src={`img/${livre.image}`}
-        alt="{livre.titre}"
-        className="absolute top-0 w-full h-full object-cover"
-        variants={etatImage}
-      />
-      <motion.div
-        className="absolute bg-neutral-50 top-0 p-4 w-full h-full"
-        variants={etatTexte}
-      >
-        <h2 className="text-lg font-[700] text-gray-500 pb-2">
-          {d(livre.titre)}
-        </h2>
-        <p className="text-sm font-[500] text-gray-500 mb-1">
-          {d(livre.auteur)}
-        </p>
-        <p className="w-full text-sm text-gray-500 overflow-y-hidden line-clamp-15">
-          {d(livre.description)}
-        </p>
-      </motion.div>
+      <div className="absolute min-h-[549px] min-w-[349px]">
+        <motion.img
+          src={`img/${livre.image}`}
+          alt="{livre.titre}"
+          className="absolute top-0 w-full h-full object-cover rounded-sm"
+          variants={etatImage}
+        />
+        <motion.div
+          className="absolute bg-neutral-50 top-0 p-4 w-full h-full rounded-lg"
+          variants={etatTexte}
+        >
+          <h2 className="text-lg font-[700] text-gray-500 pb-2">
+            {d(livre.titre)}
+          </h2>
+          <p className="text-sm font-[500] text-gray-500 mb-1">
+            {d(livre.auteur)}
+          </p>
+          <p className="w-full text-sm text-gray-500 overflow-y-hidden line-clamp-15">
+            {d(livre.description)}
+          </p>
+        </motion.div>
+      </div>
     </motion.div>
   );
 }
