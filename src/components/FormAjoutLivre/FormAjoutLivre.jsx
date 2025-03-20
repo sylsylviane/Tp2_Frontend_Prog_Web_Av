2e3022987c963a77bf60ab4c6bee433cbb2d4919
sylsylviane <syl.sylviane1@gmail.com>
2025-03-20 17:32:45 -0400

    Modifier le nombre de lignes du textarea

--- src/components/FormAjoutLivre/FormAjoutLivre.jsx
+++ src/components/FormAjoutLivre/FormAjoutLivre.jsx
@@ -226,7 +226,7 @@ function FormAjoutLivre() {
                 <textarea
                   id="description"
                   name="description"
-                  rows={3}
+                  rows={15}
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-red-400 sm:text-sm/6"
                   onChange={onInputChange}
                   value={donneesLivre.description}
