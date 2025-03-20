05e16cc292359e581c7ccf9fde39e7c4cfe0b7e0
sylsylviane <syl.sylviane1@gmail.com>
2025-03-20 13:59:46 -0400

    Créer test unitaire pour vérifier l'affichage du formulaire

---
+++ src/components/FormAjoutLivre/FormAjoutLivre.test.jsx
@@ -0,0 +1,21 @@
+import FormAjoutLivre from "./FormAjoutLivre";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { describe, expect, it } from "vitest";
+import { vi } from "vitest";
+import { useNavigate } from "react-router-dom";
+
+// Mock de useNavigate pour éviter les erreurs lors de l'exécution des tests.
+vi.mock("react-router-dom", () => ({
+  // Fonction vide pour simuler useNavigate 
+  useNavigate: vi.fn(),
+}))
+
+describe("FormAjoutLivre", () => {
+  it("affiche le formulaire d'ajout de livre", () => {
+    const mockNavigate = vi.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+    render(<FormAjoutLivre />);
+    expect(document.querySelector("form")).toBeInTheDocument();
+  });
+});
