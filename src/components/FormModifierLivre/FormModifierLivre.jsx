806682bd15f4621ff4e11664f0d05e0c3b82bcf6
sylsylviane <syl.sylviane1@gmail.com>
2025-03-20 17:03:46 -0400

    Ajouter la validation du formulaire

--- src/components/FormModifierLivre/FormModifierLivre.jsx
+++ src/components/FormModifierLivre/FormModifierLivre.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { trim } from "validator";
+import { trim, isLength, isISBN, isInt, isEmpty } from "validator";
 import he from "he";
+
 function FormModifierLivre() {
+  // Récupérer l'identifiant du livre à modifier
   const { id } = useParams();
+
+  // Référence pour le formulaire
   const formRef = useRef();
+
+  // Utiliser le hook useNavigate pour la navigation
   const navigate = useNavigate();
+
+  // Définition des états
   const [categories, setCategories] = useState([]);
+  const [formulaireValide, setFormulaireValide] = useState(false);
   const [donneesLivre, setDonneesLivre] = useState({
     titre: "",
     description: "",
@@ -30,8 +39,11 @@ function FormModifierLivre() {
     auteur: "",
   });
   const [message, setMessage] = useState("");
+
   // Fonction qui permet de décoder les caractères spéciaux
   const d = (text) => he.decode(text);
+
+  // Récupérer les détails d'un livre de la base de données
   useEffect(() => {
     async function getData() {
       try {
@@ -42,33 +54,41 @@ function FormModifierLivre() {
         }
         const reponse = await fetch(`${url}/livres/${id}`);
         const donneesLivre = await reponse.json();
-        console.log(donneesLivre.id);
 
         setDonneesLivre(donneesLivre);
         setCategories(donneesLivre.categories);
-      } catch (error) {
-        console.error("Erreur de récupération des données", error);
+      } catch (erreur) {
+        setMessage("Une erreur s'est produite. Veuillez réessayer plus tard.");
       }
     }
     getData();
   }, []);
 
+  // Mettre à jour les catégories du livre
   useEffect(() => {
     const donnees = { ...donneesLivre, categories };
     setDonneesLivre(donnees);
   }, [categories]);
 
+  /**
+   * Fonction qui permet de gérer le changement des champs du formulaire
+   * @param {*} e 
+   */
   function onInputChange(e) {
     const champ = e.currentTarget;
     const nom = champ.name;
     let valeur = champ.value;
 
-    // valeur = trim(valeur);
+    valeur = trim(valeur);
 
     const nouvellesValeurs = { ...donneesLivre, [nom]: valeur };
     setDonneesLivre(nouvellesValeurs);
   }
 
+  /**
+   * Fonction qui permet de gérer le changement de catégorie
+   * @param {*} e Événement déclenché lors de l'interaction avec une case à cocher.
+   */
   function onCategorieChange(e) {
     const checkbox = e.currentTarget;
     const valeur = checkbox.value;
@@ -89,35 +109,111 @@ function FormModifierLivre() {
     setCategories(nouvellesCategories);
   }
 
+  /**
+   * Fonction qui permet de valider le formulaire
+   */
+  function validerFormulaire() {
+      const nouvellesErreurs = {};
+      const dateCourante = new Date().getFullYear();
+  
+      if (isEmpty(donneesLivre.titre)) {
+        nouvellesErreurs.titre = "Le titre ne peut pas être vide";
+      } else if (!isLength(donneesLivre.titre, { max: 250 })) {
+        nouvellesErreurs.titre =
+          "Le titre est trop long. (maximum: 250 caractères)";      
+      }
+  
+      if (!isLength(donneesLivre.description, { max: 2000 })) {
+        nouvellesErreurs.description =
+          "La description est trop longue. (maximum: 2000 caractères)";
+      }
+  
+     if (!isLength(donneesLivre.editeur, { max: 100 })) {
+       nouvellesErreurs.editeur =
+         "L'éditeur doit contenir maximum 100 caractères.";
+     }
+  
+      if (isEmpty(donneesLivre.auteur)) {
+        nouvellesErreurs.auteur = "L'auteur ne peut pas être vide";
+      } else if (!isLength(donneesLivre.auteur, { max: 100 })) {
+        nouvellesErreurs.auteur =
+          "Le champ auteur est trop long. (maximum: 100 caractères)";
+      }
+  
+      if (isEmpty(donneesLivre.isbn)) {
+        nouvellesErreurs.isbn = "L'isbn ne peut pas être vide";
+      } else if (!isISBN(donneesLivre.isbn)) {
+        nouvellesErreurs.isbn =
+          "Veuillez entrer un ISBN valide (10 ou 13 caractères)";
+      }
+  
+      if (!isLength(donneesLivre.pages, { max: 10 })) {
+        nouvellesErreurs.page =
+          "Le nombre de page ne peut dépasser 10 caractères";
+      }
+  
+      if (!isEmpty(donneesLivre.date) && !isInt(donneesLivre.date, { min: 1900, max : dateCourante })) {
+        nouvellesErreurs.date =
+          "L'année de publication doit être une date valide entre 1900 et aujourd'hui.";
+      }
+  
+      if (isEmpty(donneesLivre.image)) {
+        nouvellesErreurs.image =
+          "Le champ image ne peut pas être vide";
+      }else if (
+        !donneesLivre.image.endsWith(".jpg") &&
+        !donneesLivre.image.endsWith(".jpeg") &&
+        !donneesLivre.image.endsWith(".png") &&
+        !donneesLivre.image.endsWith(".gif")
+      ) {
+        nouvellesErreurs.image =
+          "Veuillez entrer une image de format .jpeg, .gif, .png ou .jpg.";
+      }
+  
+      setErreurs(nouvellesErreurs); // On met à jour les erreurs
+  
+      // On vérifie si le formulaire est valide en vérifiant s'il n'y a pas d'erreurs et si le formulaire est valide selon les règles de validation de HTML 5 (required, type, etc.). 
+      setFormulaireValide(
+        formRef.current.checkValidity() &&
+          Object.keys(nouvellesErreurs).length === 0
+      );
+    }
+
+  /**
+   * Fonction qui permet de soumettre le formulaire de modification d'un livre
+   * @param {*} e 
+   */
   async function onSubmit(e) {
     e.preventDefault();
+    validerFormulaire();
+    if (formRef.current.checkValidity() && formulaireValide) {
+      try{
+        let url = import.meta.env.VITE_DEV_URL;
 
-    if (formRef.current.checkValidity()) {
-      let url = import.meta.env.VITE_DEV_URL;
+        if (import.meta.env.VITE_MODE == "PRODUCTION") {
+          url = import.meta.env.VITE_PROD_URL;
+        }
+        const objDonnees = {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(donneesLivre),
+        };
 
-      if (import.meta.env.VITE_MODE == "PRODUCTION") {
-        url = import.meta.env.VITE_PROD_URL;
+        const reponse = await fetch(`${url}/livres/${id}`, objDonnees);
+
+        if (reponse.ok) {
+          navigate(`/livres/${id}`);
+        } else {
+          setMessage(resultat.message);
+        }
+      }catch(erreur){
+        setMessage("Une erreur s'est produite. Veuillez réessayer plus tard.");
       }
-      const objDonnees = {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(donneesLivre),
-      };
-      console.log(objDonnees.body);
-
-      const reponse = await fetch(`${url}/livres/${id}`, objDonnees);
-      console.log(donneesLivre);
-
-      if (reponse.ok) {
-        navigate(`/livres/${id}`);
-      } else {
-        setMessage("Veuillez corriger le formulaire");
-        setTimeout(() => {
-          setMessage("");
-        }, 2000);
-      }
+    }
+    else{
+      setMessage("Veuillez corriger les erreurs dans le formulaire.");
     }
   }
 
@@ -307,6 +403,7 @@ function FormModifierLivre() {
           </div>
         </div>
 
+{/* SECTION CATÉGORIES - CHECKBOXES */}
         <div className="border-b border-neutral-50 pb-12 ">
           <h2 className="text-lg ">Catégories</h2>
           <p className="mt-1 text-sm/6 text-gray-400">
@@ -531,7 +628,7 @@ function FormModifierLivre() {
                         value="Littérature Americaine"
                         type="checkbox"
                         onChange={onCategorieChange}
-                        checked={categories.includes("Littérature Americaine")}
+                        checked={categories.includes("Litterature Americaine")}
                         aria-describedby="categorie-litterature-americaine"
                         className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-red-400 checked:bg-red-400 indeterminate:border-red-400 indeterminate:bg-red-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
                       />
@@ -699,17 +796,17 @@ function FormModifierLivre() {
       </div>
 
       <div className="mt-6 flex items-center justify-center gap-x-6">
-        {/* <button type="button" className="text-sm/6">
-            Annuler
-          </button> */}
-        <button className="rounded-md bg-yellow-700 px-3 py-2 text-sm font-semibold text-gray-700 shadow-xs hover:bg-red-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400">
-          Sauvegarder
-        </button>
+        <input
+          type="submit"
+          className="rounded-md bg-yellow-700 px-3 py-2 text-sm font-semibold text-gray-700 shadow-xs hover:bg-red-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400"
+          value="Sauvegarder"
+        />
         <Link to={`/livres/${id}`} className="text-sm/6">
           Retour au livre
         </Link>
       </div>
 
+      {/* Message */}
       {message ? (
         <div className="border rounded-md p-3 mt-3 ">
           <p>{message}</p>
