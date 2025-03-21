cd49733e7515af5769b645c472277654ebb6de19
sylsylviane <syl.sylviane1@gmail.com>
2025-03-21 16:09:36 -0400

    Enlever balise div inutile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,9 @@ import Nav from "../Nav/Nav";
 
 function Header() {
   return (
-    <div>
-      <header className="absolute inset-x-0 top-0 z-50">
-        <Nav />
-      </header>
-    </div>
+    <header className="absolute inset-x-0 top-0 z-50">
+      <Nav />
+    </header>
   );
 }
 
