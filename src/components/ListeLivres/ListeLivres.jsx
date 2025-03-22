532a2e708ee2342b24584f40d6f4c92ac0c045dd
sylsylviane <syl.sylviane1@gmail.com>
2025-03-22 11:33:20 -0400

    Ajouter try and catch et animation de loading

--- src/components/ListeLivres/ListeLivres.jsx
+++ src/components/ListeLivres/ListeLivres.jsx
@@ -1,26 +1,48 @@
 // TODO: IMPORTER CSS
 
 import { useState, useEffect } from "react";
+import { useAnimation } from "motion/react";
 import CarteLivre from "../CarteLivre/CarteLivre";
+import LoadingCircleSpinner from "../LoadingCircleSpinner/LoadingCircleSpinner";
 
 // Composant qui permet d'afficher la liste des livres
 function ListeLivres() {
+  const controls = useAnimation(); //Permet de gérer les animations manuellement
   // Déclaration des variables d'état
   let [livres, setLivres] = useState([]);
   const [recherche, setRecherche] = useState("");
+  let [loading, setLoading] = useState(true);
+  const [message, setMessage] = useState("");
 
   // Récupérer les livres de la base de données
   useEffect(() => {
     async function fetchData() {
-      let url = import.meta.env.VITE_DEV_URL;
+      try {
+        setLoading(true);
 
-      if (import.meta.env.VITE_MODE == "PRODUCTION") {
-        url = import.meta.env.VITE_PROD_URL;
+        let url = import.meta.env.VITE_DEV_URL;
+
+        if (import.meta.env.VITE_MODE == "PRODUCTION") {
+          url = import.meta.env.VITE_PROD_URL;
+        }
+
+        const reponse = await fetch(`${url}/livres`);
+        const donneesLivres = await reponse.json();
+        if (reponse.ok) {
+          setLivres(donneesLivres);
+          setLoading(false);
+        } else {
+          setMessage(donneesLivres.message);
+          setLoading(false);
+        }
+        //Permet d'attendre le chargement avant de déclencher l'animation
+        controls.start("visible");
+      } catch (erreur) {
+        setMessage(
+          "Erreur lors de la récupération des livres. Veuillez réessayer plus tard."
+        );
+        setLoading(false);
       }
-
-      const reponse = await fetch(`${url}/livres`);
-      const donneesLivres = await reponse.json();
-      setLivres(donneesLivres);
     }
     fetchData();
   }, []);
@@ -48,7 +70,7 @@ function ListeLivres() {
 
   /**
    *  Fonction qui permet de trier les livres par auteur
-   * @param {*} e Événement déclencheur 
+   * @param {*} e Événement déclencheur
    */
   function trierAuteur(e) {
     const declencheur = e.target;
@@ -69,6 +91,16 @@ function ListeLivres() {
 
   return (
     <>
+      {/* Message */}
+      {message ? (
+        <div className="border rounded-md p-3 mt-[150px] mx-[50px] bg-red-100 text-red-900">
+          <p>{message}</p>
+        </div>
+      ) : (
+        ""
+      )}
+      {/* Loading */}
+      {loading && <LoadingCircleSpinner />}
       <section className="flex flex-col md:flex-row gap-x-20 justify-center mx-[50px] mb-[50px]">
         <div className="mt-[150px] flex flex-col  ">
           <div>
@@ -144,83 +176,6 @@ function ListeLivres() {
           {livres.length == 0 && "Aucun livre trouvé."}
         </div>
       </section>
-
-      {/* <section className="flex flex-wrap justify-end">
-        <div className="mt-[100px] flex flex-col justify-end gap-x-10 mx-[50px] ">
-          <div>
-            <label htmlFor="recherche" className="mt-3 ">
-              Rechercher
-            </label>
-            <input
-              onChange={(e) => setRecherche(e.target.value)}
-              id="recherche"
-              type="search"
-              className="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-red-400 sm:text-sm/6"
-            />
-          </div>
-          <div className="flex gap-x-10 mt-5">
-            <div className="">
-              <label htmlFor="parTitre" className="block text-sm/6 font-medium">
-                Par titre
-              </label>
-
-              <select
-                name="ordre"
-                id="parTitre"
-                onChange={trierTitre}
-                className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-red-400 sm:text-sm/6"
-              >
-                <option value="ascendant">Ascendant</option>
-                <option value="descendant">Descendant</option>
-              </select>
-            </div>
-            <div className="">
-              <label
-                htmlFor="parAuteur"
-                className="block text-sm/6 font-medium"
-              >
-                Par auteur
-              </label>
-
-              <select
-                name="ordre"
-                id="parAuteur"
-                onChange={trierAuteur}
-                className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-red-400 sm:text-sm/6"
-              >
-                <option value="ascendant">Ascendant</option>
-                <option value="descendant">Descendant</option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="m-[50px]">
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 2xl:gap-x-15 xl:gap-10 lg:gap-10 md:gap-25 sm:gap-20 gap-y-20 ">
-          {livres
-            .filter((livre) => {
-              if (recherche == "") {
-                return livre;
-              } else if (
-                livre.auteur.toLowerCase().includes(recherche.toLowerCase())
-              ) {
-                return livre;
-              } else if (
-                livre.titre.toLowerCase().includes(recherche.toLowerCase())
-              ) {
-                return livre;
-              } else if (
-                livre.isbn.toLowerCase().includes(recherche.toLowerCase())
-              ) {
-                return livre;
-              }
-            })
-            .map((livre) => {
-              return <CarteLivre key={livre.id} livre={livre} />;
-            })}
-          {livres.length == 0 && "Aucun livre trouvé."}
-        </div>
-      </section> */}
     </>
   );
 }
