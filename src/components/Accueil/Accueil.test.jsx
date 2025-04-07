8b1bdde59daa186121e2918e26345c1a9ec47b26
sylsylviane <syl.sylviane1@gmail.com>
2025-04-07 07:56:19 -0400

    Modifier commentaire sur IntersectionObserver

--- src/components/Accueil/Accueil.test.jsx
+++ src/components/Accueil/Accueil.test.jsx
@@ -3,8 +3,9 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import {describe, it, expect } from "vitest";
 
-// Ajouter un mock pour IntersectionObserver pour éviter les erreurs
-// lors de l'exécution des tests
+//La librairie framer-motion (qui est souvent appelée Motion.js) utilise IntersectionObserver en interne pour gérer les animations basées sur la visibilité d'un élément (ex. whileInView).Pourquoi il y a des erreurs avec Vitest ?
+// Vitest ne fournit pas de IntersectionObserver par défaut dans son environnement de test (JSDOM ne l'implémente pas). Du coup, quand on utilise whileInView ou d'autres animations qui reposent sur IntersectionObserver, Vitest plante en disant qu'il ne reconnaît pas cette API.
+// Solution: mocker IntersectionObserver dans tes tests avant d'exécuter le code. 
 beforeAll(() => {
   global.IntersectionObserver = class {
     constructor() {}
